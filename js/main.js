23aa6ab642750ad79efeea2602e17febff53aae3
Sachin Jadhav <34229084+theSachinJadhav@users.noreply.github.com>
2018-05-31 12:41:47 +0530

    Initial Commit
    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Animate smooth scroll
+$("#view-work").on("click", function() {
+  const images = $("#images").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images
+    },
+    900
+  );
+});
+
+$("#top").on("click", function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    1500
+  );
+});
